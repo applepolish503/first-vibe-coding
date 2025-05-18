50b232518c7ff048e4ed388e61f793c053f84b5d
applepolish503 <applepolish503@gmail.com>
2025-05-18 15:33:03 +0900

    Add auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flow-editor-sample/",
+  base: "/first-vibe-coding/",
 });
